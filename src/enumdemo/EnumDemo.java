3c8303e1dd874074276ea1af3af9f82602a780bd
Anjali Choudhary <anjalic@maveric-systems.com>
2023-10-06 12:31:33 +0530

    Added Expections demo

--- src/enumdemo/EnumDemo.java
+++ src/enumdemo/EnumDemo.java
@@ -4,23 +4,39 @@ import com.sun.security.jgss.GSSUtil;
 
 public class EnumDemo {
     public static void main(String[] args) {
-        DayBreak morningBreak=DayBreak.MorningTeaBreak; //static variable
-        DayBreak lunchBreak=DayBreak.LunchBreak;
-        DayBreak eveningBreak=DayBreak.EveningTeaBreak;
-        System.out.println("Lunchbreak="+lunchBreak+" "+lunchBreak.ordinal());
+        EnumDemo demo = new EnumDemo();
+        DayBreak morningBreak = DayBreak.MorningTeaBreak; //static variable
+        DayBreak lunchBreak = DayBreak.LunchBreak;
+        DayBreak eveningBreak = DayBreak.EveningTeaBreak;
+        System.out.println("Lunchbreak=" + lunchBreak + " " + lunchBreak.ordinal());
         //if i wanna iterate over these
-        DayBreak values[]=DayBreak.values();
+        DayBreak values[] = DayBreak.values();
         System.out.println("************Printing the values of daybreak can have***********");
-        for (DayBreak iteratedBreak:values){
-            System.out.println(iteratedBreak+" "+"position="+iteratedBreak.ordinal());
-            System.out.println("Starttime="+" "+iteratedBreak.getStartTime());
-            System.out.println("Duration="+" "+iteratedBreak.getDurationInMinutes());
+        for (DayBreak iteratedBreak : values) {
+            System.out.println(iteratedBreak + " " + "position=" + iteratedBreak.ordinal());
+            System.out.println("Starttime=" + " " + iteratedBreak.getStartTime());
+            System.out.println("Duration=" + " " + iteratedBreak.getDurationInMinutes());
             System.out.println("***********************************************************");
 
         }
-        DayBreak morningBreak1=DayBreak.MorningTeaBreak;
-        DayBreak morningBreak2=DayBreak.MorningTeaBreak;
-        boolean isSameObj=morningBreak2==morningBreak1;
+        DayBreak morningBreak1 = DayBreak.MorningTeaBreak;
+        DayBreak morningBreak2 = DayBreak.MorningTeaBreak;
+        boolean isSameObj = morningBreak2 == morningBreak1;
         System.out.println(isSameObj);
+        DayBreak foundLunchBreak = demo.getBreak("lunchbreak");
+        System.out.println("Starts at=" + " " + foundLunchBreak.getStartTime() + " " + "duration" + foundLunchBreak);
+
+    }
+
+    DayBreak getBreak(String breakType) {
+        for (DayBreak iteratedBreak :DayBreak.values()){
+
+            if (iteratedBreak.name().toLowerCase().equals(breakType.toLowerCase())) {
+                return iteratedBreak;
+
+            }
+
+        }
+        return null;
     }
 }
--- src/exceptiondemos/ExDemo1.java
+++ src/exceptiondemos/ExDemo1.java
@@ -1,2 +1,47 @@
-package exceptiondemos;public class ExDemo1 {
+package exceptiondemos;
+
+public class ExDemo1 {
+    public static void main(String[] args) {
+        ExDemo1 demo=new ExDemo1();
+        String input="";
+
+        String result= demo.getBlockLetterString(input);
+        //System.out.println();
+//        String upperCased=substring.toUpperCase();
+        System.out.println("input "+input);
+System.out.println("result="+" "+result);
+        //String employees[]={"kushal","santosh","rachna"};
+    }
+
+    //get substring starting from s and ending at n
+    //get substring from 3 ending 6
+
+    public String getBlockLetterString(String input){
+//        String found=null;
+//        for(int i =0;i<input.length();i++)
+//        {
+//            char startchar=input.charAt(i);
+//
+//        }
+       try {
+           char firstChar = input.charAt(0);
+           char capitalCase = Character.toUpperCase(firstChar);
+           String desired = capitalCase + input.substring(1);
+           return desired;
+           //String input.substring(3,6);
+      }
+       catch (NullPointerException e){
+           System.out.println("input is null");
+           return null;
+       }
+       catch (StringIndexOutOfBoundsException e){
+           System.out.println(e.getMessage());
+           return null;
+       }
+       catch (Exception e){
+           System.out.println("something went wrong");
+           return null;
+       }
+
+    }
 }
